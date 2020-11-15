f623f0323e83950b94b9835e37df5d5335ad983c
Ryan Mottola <rmottola68@gmail.com>
2020-11-15 01:54:12 -0500

    included small style.css, cleaned up comments

--- script.js
+++ script.js
@@ -3,15 +3,8 @@ $(document).ready(function () {
     $('#search-button').on('click', () => {
         let searchTerm = $('#search-field').val();
         let url = "https://api.giphy.com/v1/gifs/search?api_key=FoVxkgXu1a1oBtFmJhHjo3YC7PtgtcPb&q=" + searchTerm;
-        // jquery way of doin it
         $.get(url, function(giphyResponse) {
-        console.log(giphyResponse);
-        console.log('giphyData', giphyResponse.data)
-        console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
-        console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
-        console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
-        console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
- 
+         
         let resultsHtml = '';
         for(let image of giphyResponse.data){
             let imgHtml = `<img src=${image.images.original.url} />`
